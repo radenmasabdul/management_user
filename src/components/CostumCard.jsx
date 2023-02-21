168962ac8e44a890ffe6ab5769cd51805531c970
radenmasabdul <abdul.tamsis@gmail.com>
2023-02-21 23:07:24 +0700

    get all user list

--- src/components/CostumCard.jsx
+++ src/components/CostumCard.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import axios from "axios";
 
 import Search from "./Search";
 import ButtonLoadMore from "./ButtonLoadMore";
+import Pagination from "./Pagination";
 import { ModalAddData, ModalViewData } from "./ModalAddData";
 
 const CostumCard = () => {
@@ -71,6 +75,22 @@ const CostumCard = () => {
 };
 
 const CardMasterUser = () => {
+  const [user, setUser] = useState([]);
+  const [page, setPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    getAllUser(page);
+  }, [page]);
+
+  const getAllUser = async (page) => {
+    const response = await axios.get(
+      `https://gorest.co.in/public/v2/users?page=${page}`
+    );
+    console.log(response.data);
+    setUser(response.data);
+  };
+
   return (
     <>
       <div className="flex flex-wrap gap-2 justify-between">
@@ -90,43 +110,28 @@ const CardMasterUser = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>CyGanderton@mail.co</td>
-              <td>Male</td>
-              <td>Active</td>
-              <td>
-                <ModalViewData />
-              </td>
-            </tr>
-
-            <tr>
-              <th>2</th>
-              <td>Cy Ganderton</td>
-              <td>CyGanderton@mail.co</td>
-              <td>Male</td>
-              <td>Active</td>
-              <td>
-                <ModalViewData />
-              </td>
-            </tr>
-
-            <tr>
-              <th>3</th>
-              <td>Cy Ganderton</td>
-              <td>CyGanderton@mail.co</td>
-              <td>Male</td>
-              <td>Active</td>
-              <td>
-                <ModalViewData />
-              </td>
-            </tr>
+            {user &&
+              user.map((users, index) => (
+                <tr key={users.id}>
+                  <th>{index + 1}</th>
+                  <td>{users.name}</td>
+                  <td>{users.email}</td>
+                  <td>{users.gender}</td>
+                  <td>{users.status}</td>
+                  <td>
+                    <ModalViewData />
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
       <div className="flex flex-wrap justify-center py-5">
-        <ButtonLoadMore />
+        <Pagination
+          nPages={page}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
     </>
   );
